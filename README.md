b299a940f5ffb1ff4f69f16dc4634900d22b6844
Pedro Cruz <pedro.cruz.seguros@gmail.com>
2024-11-22 17:42:02 +0000

    implement url shortener and qr code generator

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-In progress
+Pratice project
+
+A Url shortener with a qr code generator
+
+APIs used:
+
+- url shortener: https://spoo.me/api
+- qrCode generator: https://goqr.me/api/
+
+HTML, CSS, Javascript, Node.js, EJS
+
+How to run:
+1. Open root file in code editor ;
+2. cd to root directory ;
+3. run npm install ;
+4. deal with the process.env (see the point 6 below);
+5. run it with " nodemon app.js ";
+
+6. if you will only run locally, in the app.js file just replace all the process.env. variables (line 9 and 10 only) with the api base Url's.
+If you prefer to hide those Urls, leave the app.js as it is and instead, in the root directory of the project create a file named " .env " , copy the content of example.env file , paste it into your created .env file and assign your Urls to the specific variables. Note that if you use different APIs , the get requests will need, most likely to be changed accordingly to your chosen APIs.
--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 import express from "express";
 import bodyParser from "body-parser";
 import axios from "axios";
+import env from "dotenv";
 
+env.config();
 const app = express();
 const port = 3000;
-const APIURL = "https://spoo.me"
-
-const config = {
-    
-};
+const apiUrlShortener = process.env.URL_SHORTENER_API ;
+const apiQrCodeGen = process.env.QR_CODE_GENERATOR_API ;
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
+///////
 
 app.get("/", (req, res) => {
    res.render("index.ejs")
@@ -19,30 +21,55 @@ app.get("/", (req, res) => {
 
 app.post("/submit", async (req, res) => {
     const input = req.body;
-    const data = {
+    const urlShortenerData = {
         url: input.longUrl,
         alias: input.alias,
     };
     
+    // Get short Url
     try {
-        const response = await axios.post( APIURL , data, {
+        const response = await axios.post( apiUrlShortener , urlShortenerData, {
             headers: {
                 Accept: "application/json",
                 'Content-Type': "application/x-www-form-urlencoded",
             },
         });
-        const result = response.data.shortUrl;
+        const shortUrlResult = response.data.short_url;
 
-        res.render("result.ejs", {
-            shortUrl: result,
-        });
+            // Get Qr Code from the shortened url
+            try {
+                const QRcodeResponse = await axios.get( apiQrCodeGen, {
+                    params: {
+                        data: shortUrlResult,
+                        size: "150x150",
+                        format: "png",
+                    }
+                } );
+                
+                const qrParams = QRcodeResponse.config.params;
+                const qrCodeSrc = apiQrCodeGen + "?data=" + qrParams.data + "&size=" + qrParams.size + "&format=" + qrParams.format;
+
+                res.render("result.ejs", {
+                    shortUrl: shortUrlResult,
+                    qrCodeUrl: qrCodeSrc,
+                });
+
+            } catch (error) {
+                console.log("Error getting the Qr Code. ", error)
+            };
         
     } catch (error) {
-        console.error(error);
-        console.log("Error ", error.message)
-    }
-})
+        console.error("Error getting the short URL: ", error);
 
+        res.render("index.ejs", {
+            errorMessage: error.response.data.AliasError,
+        });
+    };
+});
+
+app.get("/try-again", (req, res) => {
+    res.redirect("/");
+});
 
 app.listen(port, () => {
  console.log("Server listening on port " + port);
